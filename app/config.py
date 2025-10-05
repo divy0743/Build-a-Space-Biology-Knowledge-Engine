0a3d2654e4efcdd543337519d4db4c43c4289f82
divy0743 <kharedivyanshi86@gmail.com>
2025-10-05 10:51:58 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,2 +1,13 @@
 # Mongo DB Connection + env config
 
+from dotenv import load_dotenv
+import os
+from motor.motor_asyncio import AsyncIOMotorClient
+
+load_dotenv()  # loads .env file into environment variables
+
+MONGO_URI = os.getenv("mongodb+srv://divyanshikhare37_db_user:3OnM4mWUoH02B9OL@cluster0.vwumbxa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&authSource=admin")
+DB_NAME = os.getenv("nasa_db")
+
+client = AsyncIOMotorClient(MONGO_URI)
+db = client[DB_NAME]
